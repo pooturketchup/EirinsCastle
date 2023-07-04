bd5bb4e1efe6f8b5a5ca513ff50a67e9b5158c8a
Nolan Mortenson <nolan.mortenson@gmail.com>
2023-07-04 14:22:42 -0400

    adding testing scene with basic player character and floor with walls

---
+++ Eirins-Castle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput,verticalInput) * speed;
+        rb.velocity = movement;
+    }
+}
